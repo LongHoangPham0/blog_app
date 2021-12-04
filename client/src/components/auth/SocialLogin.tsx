45c8d5694e2f616dc7218f9f38dd5e2b1d88f305
Shenlong <lshen649@gmail.com>
2021-12-04 21:40:16 +0700

    make it better

---
+++ client/src/components/auth/SocialLogin.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { FacebookLogin, FacebookLoginAuthResponse } from 'react-facebook-login-lite'
+import { GoogleLogin, GoogleLoginResponse } from 'react-google-login-lite'
+
+import { googleLogin, facebookLogin } from '../../redux/actions/authAction'
+
+const SocialLogin = () => {
+    const dispatch = useDispatch()
+
+    const onSuccess = (googleUser: GoogleLoginResponse) => {
+        //console.log(googleUser);
+        const id_token = googleUser.getAuthResponse().id_token
+        dispatch(googleLogin(id_token))
+    }
+
+    const onFBSuccess = (response: FacebookLoginAuthResponse) => {
+        const { accessToken, userID } = response.authResponse
+        // console.log({ accessToken, userID })
+        dispatch(facebookLogin(accessToken, userID))
+    }
+
+    return (
+        <>
+            <div className='my-2'>
+                <GoogleLogin 
+                    client_id='548466254886-h56eslmprphtvkuisnlumlp6o2ivbqjq.apps.googleusercontent.com'
+                    cookiepolicy='single_host_origin'
+                    onSuccess={onSuccess}
+                />
+            </div>
+
+            <div className='my-2'>
+                <FacebookLogin 
+                    appId="333953021722051"
+                    onSuccess={onFBSuccess}
+                />
+            </div>
+        </>
+    )
+}
+
+export default SocialLogin
