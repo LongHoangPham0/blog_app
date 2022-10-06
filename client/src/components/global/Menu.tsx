9df1a0074472db5424714b428359715e8800273e
LongHoangPham0 <51282273+LongHoangPham0@users.noreply.github.com>
2022-10-06 11:09:49 +0700

    make it better

--- client/src/components/global/Menu.tsx
+++ client/src/components/global/Menu.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootStore } from '../../utils/TypeScript'
